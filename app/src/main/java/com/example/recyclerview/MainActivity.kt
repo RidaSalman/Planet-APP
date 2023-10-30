cd3ce8f943981e8891a2f1ec2db990026454d027
Rida Salman <ridasalman458@gmail.com>
2023-10-30 13:30:48 +0500

    splash screen

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Recyclerview)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
