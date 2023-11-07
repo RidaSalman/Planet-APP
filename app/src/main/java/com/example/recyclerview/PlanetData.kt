6cdabcb24e33312898064acc692d600fa5c7b280
Rida Salman <ridasalman458@gmail.com>
2023-11-07 15:17:44 +0500

    Added planet card layout XML.

---
+++ app/src/main/java/com/example/recyclerview/PlanetData.kt
@@ -0,0 +1,13 @@
+package com.example.recyclerview
+
+import android.view.Gravity
+
+data class PlanetData(
+    val id : Int?,
+    val title:String?,
+    val galaxy:String?,
+    val distance:String?,
+    val gravity:String?,
+    val overview:String?,
+
+)
